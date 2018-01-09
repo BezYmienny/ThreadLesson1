9081beb7bdd75a256edc785213acb889239b8353
BezYmienny <mirek.piotr@gmail.com>
2018-01-09 22:04:58 +0100

    1st lesson with Threads - How to skip thread and switch to another?

--- ConsoleApplicationThread1/Program.cs
+++ ConsoleApplicationThread1/Program.cs
@@ -15,24 +15,28 @@ namespace ConsoleApplicationThread1
         static void Main(string[] args)
         {
             Thread something = new Thread(new ThreadStart(Encounting));
-            something.Start();
-
+            
             Thread something2 = new Thread(new ThreadStart(Encounting2));
-            something2.Start();
-
+            
             Thread something3 = new Thread(new ThreadStart(Encounting3));
+
+            something.Start();
+            something2.Start();
             something3.Start();
+            Thread.Sleep(0);
 
-
-            for (int y = 10; y < 100; y++)
+            for (int y = 1; y < 100; y++)
             {
                 Console.WriteLine("Main loop {0}", y);
 
-                if (IsOdd(y / 10))
-                    something.Join();
-                else
+                if (IsThird(y))
+                    something3.Join();
+                else if (IsOdd(y))
                     something2.Join();
-                // test branch
+                else
+                    something.Join();
+
+                Thread.Sleep(0);
             }
             Console.ReadKey();
 
@@ -46,12 +50,20 @@ namespace ConsoleApplicationThread1
             return result;
     }
 
+        public static bool IsThird(int NumberForTest)
+        {
+            bool result = false;
+            if (NumberForTest % 3 == 0)
+                result = true;
+            return result;
+        }
+
         public static void Encounting()
         {
             for (int i=0; i < 100; i++)
             {
                 Console.WriteLine("Thread result \tE{0}", i);
-                Thread.Sleep(2);
+                Thread.Sleep(0);
             }
         }
         public static void Encounting2()
@@ -59,7 +71,7 @@ namespace ConsoleApplicationThread1
             for (int i = 0; i < 100; i++)
             {
                 Console.WriteLine("Thread result \t\tE2{0}", i);
-                Thread.Sleep(3);
+                Thread.Sleep(0);
             }
         }
 
@@ -68,7 +80,7 @@ namespace ConsoleApplicationThread1
             for (int i = 0; i < 100; i++)
             {
                 Console.WriteLine("Thread result \t\t\tE3{0}", i);
-                Thread.Sleep(3);
+                Thread.Sleep(0);
             }
         }
     }
