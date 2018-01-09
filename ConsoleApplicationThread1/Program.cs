5e8fb579c26b6524e8fabf396ebbbb8cdef4f460
BezYmienny <mirek.piotr@gmail.com>
2018-01-09 21:23:20 +0100

    1st lesson with Threads

--- ConsoleApplicationThread1/Program.cs
+++ ConsoleApplicationThread1/Program.cs
@@ -3,13 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace ConsoleApplicationThread1
 {
+
+    // Thius app is my excercises with Thread Class
+
     class Program
     {
         static void Main(string[] args)
         {
+            Thread something = new Thread(new ThreadStart(Encounting));
+            something.Start();
+
+            Thread something2 = new Thread(new ThreadStart(Encounting2));
+            something2.Start();
+
+
+            for (int y = 10; y < 100; y++)
+            {
+                Console.WriteLine("Main loop {0}", y);
+
+                if (IsOdd(y / 10))
+                    something.Join();
+                else
+                    something2.Join();
+
+            }
+            Console.ReadKey();
+
+        }
+
+    public static bool IsOdd(int NumberForTest)
+    {
+            bool result = false;
+            if (NumberForTest % 2 == 0)
+                result =true;
+            return result;
+    }
+
+        public static void Encounting()
+        {
+            for (int i=0; i < 100; i++)
+            {
+                Console.WriteLine("Thread result \tE{0}", i);
+                Thread.Sleep(2);
+            }
+        }
+        public static void Encounting2()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("Thread result \t\tE2{0}", i);
+                Thread.Sleep(3);
+            }
         }
     }
 }
