a4b7ddc7a6e36b857e69857b9636e3aed45b33f2
BezYmienny <mirek.piotr@gmail.com>
2018-01-09 22:23:29 +0100

    branch ParamThread : enjoy IsBackground = true/false;

--- ConsoleApplicationThread1/Program.cs
+++ ConsoleApplicationThread1/Program.cs
@@ -9,89 +9,29 @@ namespace ConsoleApplicationThread1
 {
 
     // This app is my excercises with Thread Class
-    // In my opinion threads are much unpredictable - I can't force to jump from thread to thread
+   
 
     class Program
     {
         static void Main(string[] args)
         {
-            Thread something = new Thread(new ThreadStart(Encounting));
-            
-            Thread something2 = new Thread(new ThreadStart(Encounting2));
-            
-            Thread something3 = new Thread(new ThreadStart(Encounting3));
-
+            Thread something = new Thread(new ThreadStart(ThreadMethod));
+            something.IsBackground = true;
             something.Start();
-            something2.Start();
-            something3.Start();
-            //Thread.Sleep(0);
-
-            for (int y = 1; y < 10; y++)
-            {
-                Console.WriteLine("Main loop {0}", y);
-
-                if (IsThird(y))
-                {
-                    something3.Join();
-                    Thread.Sleep(0);
-                }
-                else if (IsOdd(y))
-                {
-                    something2.Join();
-                    Thread.Sleep(0);
-                }
-                else
-                {
-                    something.Join();
-                    Thread.Sleep(0);
-                }
-
-               // Thread.Sleep(0);
-            }
-            Console.ReadKey();
+            //Console.ReadKey();
 
         }
 
-    public static bool IsOdd(int NumberForTest)
-    {
-            bool result = false;
-            if (NumberForTest % 2 == 0)
-                result =true;
-            return result;
-    }
+   
 
-        public static bool IsThird(int NumberForTest)
+        public static void ThreadMethod()
         {
-            bool result = false;
-            if (NumberForTest % 3 == 0)
-                result = true;
-            return result;
-        }
-
-        public static void Encounting()
-        {
-            for (int i=0; i < 100; i++)
+            for (int i=0; i < 10; i++)
             {
-                Console.WriteLine("Thread result \tE{0}", i);
-                Thread.Sleep(0);
+                Console.WriteLine("Thread Proc \t {0}", i);
+                Thread.Sleep(1000);
             }
         }
-        public static void Encounting2()
-        {
-            for (int i = 0; i < 100; i++)
-            {
-                Console.WriteLine("Thread result \t\tE2{0}", i);
-                Thread.Sleep(0);
-            }
-        }
-
-        public static void Encounting3()
-        {
-            for (int i = 0; i < 100; i++)
-            {
-                Console.WriteLine("Thread result \t\t\tE3{0}", i);
-                Thread.Sleep(0);
-            }
-        }
+      
     }
 }
