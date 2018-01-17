aae388087bcc7e9b6086b48e615289715cb7acca
BezYmienny <mirek.piotr@gmail.com>
2018-01-17 21:40:47 +0100

    2nd lesson with Thread - how StoppingThread coul I use

--- ConsoleApplicationThread1/Program.cs
+++ ConsoleApplicationThread1/Program.cs
@@ -14,37 +14,30 @@ namespace ConsoleApplicationThread1
     class Program
     {
 
-        [ThreadStatic]
-        public static int _field;
         static void Main(string[] args)
         {
-            Thread something = new Thread(new ParameterizedThreadStart(ThreadMethod));
-            //something.IsBackground = true;
-            something.Start(5);
-            (new Thread(new ParameterizedThreadStart(ThreadMethod))).Start(6);
-            (new Thread(new ParameterizedThreadStart(ThreadMethod))).Start(7);
+            bool stopped = false;
+
+            Thread t= new Thread(new ThreadStart ( () =>
+            {
+
+                while(!stopped)
+                {
+                    Console.WriteLine("Running .....");
+                    Thread.Sleep(1000);
+                }
+                
+            }));
+
+            t.Start();
+            Console.WriteLine("Press oany key to exit");
             Console.ReadKey();
 
+            stopped = true;
+            t.Join();
+            
         }
 
    
-
-        public static void ThreadMethod(object o)
-        {
-            //int param = (int)o;
-            Console.WriteLine("not assigned _field \t {0}",_field);
-            if (_field == 0)
-            {
-                _field=(int)o;
-            }
-
-            for (int i=1; i < (int)o; i++)
-            {
-                Console.WriteLine("Thread Proc \t {0} from {1} and _field {2}", i,(int)o,_field);
-                Thread.Sleep(1000);
-            }
-
-        }
-      
     }
 }
